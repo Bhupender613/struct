6a17563c6b0a2206aa8020004c3aa1b8defb1c1b
bhupender <114127045+Bhupender613@users.noreply.github.com>
2024-01-01 16:25:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000; // You can change the port if needed
+const port = 4000; // You can change the port if needed
 
 app.get('/', (req, res) => {
   res.send('Hello, World!'); // Sends 'Hello, World!' as the response
